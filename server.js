bb1cd51ffb9755bc3b90e9cf64dbb3b14d40fbbc
Youssef Ouali <yoouali@e2r4p9.1337.ma>
2022-04-15 07:01:16 +0000

    set welcome, new user, desconnect messages && get submit message

--- server.js
+++ server.js
@@ -12,7 +12,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Run when client connects
 io.on('connection', socket => {
-    console.log('New Ws Connection...')
+
+    // Welcome current user
+    socket.emit('message', 'Welcome to ChatRom!');
+
+    // Broadcast when a user connects
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    })
 });
 
 const PORT = 3000 || process.env.PORT;
